3480c64d6f321fc839205ab10d2f94ae5d73d805
Dominik Tyc <Domikxf@gmail.com>
2020-05-19 18:22:42 +0200

    copy folder with subfolders

--- Models/FileManager.cs
+++ Models/FileManager.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Xaml;
 
 namespace MiniTC.Models
 {
@@ -11,10 +8,51 @@ namespace MiniTC.Models
     {
         public void Copy(string source, string fileName, string destination)
         {
-            string sourceFile = Path.Combine(source, fileName);
-            string destFile = Path.Combine(destination, fileName);
+            
+            if (fileName.Contains("<D>"))
+            {
+                fileName = fileName.Substring(3);
+                var sourcePath = Path.Combine(source, fileName);
+                var destPath = Path.Combine(destination, fileName);
+                DirectoryCopy(sourcePath, destPath);
+            }
+            else
+            {
+                var destPath = Path.Combine(destination, fileName);
+                var sourcePath = Path.Combine(source, fileName);
+                FileCopy(sourcePath, destPath);
+            }
+        }
 
-            File.Copy(sourceFile, destFile, true);
+        private void FileCopy(string sourceFile, string destFile)
+        => File.Copy(sourceFile, destFile, true);
+
+        //https://docs.microsoft.com/en-us/dotnet/standard/io/how-to-copy-directories
+        private void DirectoryCopy(string sourceFile, string destFile)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceFile);
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destFile))
+            {
+                Directory.CreateDirectory(destFile);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destFile, file.Name);
+                file.CopyTo(temppath, false);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            // If copying subdirectories, copy them and their contents to new location.
+            foreach (DirectoryInfo subdir in dirs)
+            {
+                string temppath = Path.Combine(destFile, subdir.Name);
+                DirectoryCopy(subdir.FullName, temppath);
+            }
         }
     }
 }
